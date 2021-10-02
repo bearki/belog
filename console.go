b493e3d098b07d96716a349f29be443740ffb271
bearki <bearki@126.com>
2021-10-02 23:09:36 +0800

    add console print color

--- console.go
+++ console.go
@@ -9,6 +9,8 @@ package belog
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 	"sync"
 )
 
@@ -18,6 +20,17 @@ type consoleEngine struct {
 
 // consolePrintLock 控制台打印锁
 var consolePrintLock sync.Mutex
+
+// 全局控制台颜色
+const (
+	colorReset  = "\033[0m"  // 重置
+	colorGray   = "\033[37m" // 白色
+	colorBlue   = "\033[34m" // 蓝色
+	colorGreen  = "\033[32m" // 绿色
+	colorYellow = "\033[33m" // 黄色
+	colorRed    = "\033[31m" // 红色
+	colorViolet = "\033[35m" // 紫色
+)
 
 // initConsoleEngine 初始化控制台引擎
 func initConsoleEngine(optin interface{}) *consoleEngine {
@@ -31,6 +44,27 @@ func (consolelog *consoleEngine) printConsoleLog(logStr string) {
 	consolePrintLock.Lock()
 	// 解锁
 	defer consolePrintLock.Unlock()
+	// 提取日志级别
+	compileRegex := regexp.MustCompile(`\[(T|D|I|W|E|F){1}\]`) // 正则表达式的分组，以括号()表示，每一对括号就是我们匹配到的一个文本，可以把他们提取出来。
+	levelStr := compileRegex.FindString(logStr)
+	// 根据级别赋值颜色
+	var levelColorStr string
+	switch levelStr {
+	case `[T]`: // 通知级别(灰色)
+		levelColorStr = colorGray + levelStr + colorReset
+	case `[D]`: // 调试级别(蓝色)
+		levelColorStr = colorBlue + levelStr + colorReset
+	case `[I]`: // 普通级别(绿色)
+		levelColorStr = colorGreen + levelStr + colorReset
+	case `[W]`: // 警告级别(黄色)
+		levelColorStr = colorYellow + levelStr + colorReset
+	case `[E]`: // 错误级别(红色)
+		levelColorStr = colorRed + levelStr + colorReset
+	case `[F]`: // 紧急级别(紫色)
+		levelColorStr = colorViolet + levelStr + colorReset
+	}
+	// 替换需要加颜色的部分
+	logStr = strings.Replace(logStr, levelStr, levelColorStr, 1)
 	// 打印到控制台
 	fmt.Println(logStr)
 }
--- test/main.go
+++ test/main.go
@@ -4,12 +4,12 @@ import "github.com/bearki/belog"
 
 func main() {
 	// 直接使用方式
-	// belog.Trace("this is a trace log")
-	// belog.Debug("this is a debug log")
-	// belog.Info("this is a info log")
-	// belog.Warn("this is a warn log")
-	// belog.Error("this is a error log")
-	// belog.Fatal("this is a fatal log")
+	belog.Trace("this is a trace log")
+	belog.Debug("this is a debug log")
+	belog.Info("this is a info log")
+	belog.Warn("this is a warn log")
+	belog.Error("this is a error log")
+	belog.Fatal("this is a fatal log")
 
 	// 通过实例方式
 	// 初始化一个实例(控制台引擎记录日志)
@@ -26,7 +26,8 @@ func main() {
 			belog.LevelError,
 			belog.LevelFatal,
 		).
-		OpenFileLine()
+		OpenFileLine().
+		SetEngine(belog.EngineConsole, nil)
 	mylog.Trace("this is a trace log")
 	mylog.Debug("this is a debug log")
 	mylog.Info("this is a info log")
