9963988c060cf6f1fb13358c65f1d967f40a5231
bearki <bearki@126.com>
2021-10-20 23:23:34 +0800

    重写测试案例

--- test/main.go
+++ test/main.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	"github.com/bearki/belog"
+	"github.com/bearki/belog/console"
 	"github.com/bearki/belog/file"
 	"github.com/bearki/belog/logger"
 )
 
-// 全局日志对象
-var Log logger.Logger
-
 func main() {
-	// 配置默认实现
-	belog.OpenFileLine() // 开启行号记录
+	// 1、默认方式
+	defaultLogPrint()
+	// 2、实例方式
+	newLogPrint()
+}
+
+// 1、默认方式输出日志
+func defaultLogPrint() {
+	// 配置主引擎
+	err := belog.SetEngine(new(file.Engine), file.Options{
+		LogPath: "./logs/test_default.log",
+		MaxSize: 10,
+		SaveDay: 7,
+		Async:   false,
+	})
+	if err != nil {
+		panic(err)
+	}
+	// 配置次引擎（次引擎配置失败不影响主引擎）
+	err = belog.SetEngine(new(console.Engine), nil)
+	if err != nil {
+		belog.Error("次引擎配置失败不影响主引擎: %s", err.Error())
+	}
+	// 开启行号记录
+	belog.OpenFileLine()
+	// 指定需要记录的日志级别（默认全部级别）
+	belog.SetLevel(
+		logger.LevelTrace,
+		logger.LevelDebug,
+		logger.LevelInfo,
+		logger.LevelWarn,
+		logger.LevelError,
+		logger.LevelFatal,
+	)
 	// 打印日志
 	belog.Trace("this is a trace log")
 	belog.Debug("this is a debug log")
@@ -19,17 +49,19 @@ func main() {
 	belog.Warn("this is a warn log")
 	belog.Error("this is a error log")
 	belog.Fatal("this is a fatal log")
+}
 
-	// 实例方式
+// 2、实例方式输出日志
+func newLogPrint() {
 	// 初始化一个实例(可实例化任意引擎)
 	mylog, err := belog.New(
 		new(file.Engine), // 初始化文件引擎
 		file.Options{
-			LogPath:      "C:\\Users\\Bearki\\Desktop\\BeLog/logs/app.log", // 日志储存路径
-			MaxSize:      128,                                              // 日志单文件大小
-			SaveDay:      7,                                                // 日志保存天数
-			Async:        false,                                            // 开启异步写入(main函数提前结束会导致日志未写入)
-			AsyncChanCap: 20,                                               // 异步缓存管道容量
+			LogPath:      "./logs/test_new.log", // 日志储存路径
+			MaxSize:      128,                   // 日志单文件大小
+			SaveDay:      7,                     // 日志保存天数
+			Async:        false,                 // 开启异步写入(main函数提前结束会导致日志未写入)
+			AsyncChanCap: 20,                    // 异步缓存管道容量
 		},
 	)
 	if err != nil { // 初始化失败将不能执行任何后续操作，否则会引起恐慌
